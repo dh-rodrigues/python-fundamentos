58231e944339f9e5fd7068472cf6025699e952c6
Danilo Rodrigues <devdanrodrigues@gmail.com>
2026-05-30 18:36:17 -0300

    Adicionando Cores

---
+++ Curso_Python (Exercícios)/cor.py
@@ -0,0 +1,7 @@
+#Aprendendo a como adicionar cores
+print('\033[0;30;41mTexto de teste\033[m')
+print('\033[4;33;44mTexto de teste\033[m')
+print('\033[1;35;43mTexto de teste\033[m')
+print('\033[0;30;42mTexto de teste\033[m')
+print('\033[mTexto de teste') #volta pro padrão do terminal
+print('\033[7;30mTexto de teste\033[m')
