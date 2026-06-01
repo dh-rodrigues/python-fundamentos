cfa9c5597cf5ae12235eb6f7fc86fd651f7b48b0
Danilo Rodrigues <devdanrodrigues@gmail.com>
2026-06-01 18:25:21 -0300

    Simulação de empréstimo com uso de estruturas condicionais

--- Curso_Python (Exercícios)/exercício36.py
+++ Curso_Python (Exercícios)/exercício36.py
@@ -1,5 +1,10 @@
 #Exercício Python 36: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
 print('\033[1;37;40m                $ SIMULAÇÃO DE EMPRÉSTIMO $                \033[m')
-casa = float(input('\033[;33;44mQual o valor da casa que você deseja comprar?\033[m R$'))
-salario = float(input('\033[;33;44mQual o seu salário atual?\033[m R$ '))
-anos_pagamento = int(input('\033[;33;44mEm quantos anos deseja realizar o pagamento?\033[m R$ '))
+valor_casa = float(input('Qual o valor da casa que você deseja comprar? R$'))
+salario = float(input('Qual o seu salário atual? R$ '))
+anos_pagamento = int(input('Em quantos anos deseja realizar o pagamento? '))
+prestacao_mensal = valor_casa/(anos_pagamento*12) #tranforma ano em meses e divide do valor da casa
+if prestacao_mensal > 0.3*salario:
+    print(f'\033[;37;41mEMPRÉSTIMO NEGADO\033[m \nO valor da prestação mensal é R${prestacao_mensal:.2f} o que excede 30% do seu salário')
+else:
+    print(f'\033[;37;42mEMPRÉSTIMO CONCEDIDO\033[m \nO valor da prestação mensal é R${prestacao_mensal:.2f} o que é menor que 30% do seu salário.')
